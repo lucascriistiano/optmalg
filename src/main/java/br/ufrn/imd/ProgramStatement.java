2681ea465d7bf42ab937f33ddef829291ba9cff4
Felipe Barbalho <felipebarbalho_12@hotmail.com>
2016-05-10 10:31:21 -0300

    Implementation of BasicBlock and ProgramStatement

---
+++ src/main/java/br/ufrn/imd/ProgramStatement.java
@@ -0,0 +1,18 @@
+package br.ufrn.imd;
+
+public class ProgramStatement {
+	
+	private String statement;
+	
+	public ProgramStatement(String statement){
+		this.statement = statement;
+	}
+
+	public String getStatement() {
+		return statement;
+	}
+
+	public void setStatement(String statement) {
+		this.statement = statement;
+	}
+}
